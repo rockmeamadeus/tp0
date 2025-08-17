315701854e149475869151ea7e414adbf010cab4
rockmeamadeus <avaca@frba.utn.edu.ar>
2025-08-17 20:19:44 -0300

    Implements server socket initialization and client handling
    Replaces placeholder assertions with proper TCP server setup using
    standard networking APIs (getaddrinfo/bind/listen/accept). Adds
    robust error handling and logging, enabling real client connections
    and further development/testing. Preserves original template
    comments in the code.

--- server/src/utils.c
+++ server/src/utils.c
@@ -1,13 +1,23 @@
-#include"utils.h"
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 t_log* logger;
 
 int iniciar_servidor(void)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	// assert(!"no implementado!");
 
-	int socket_servidor;
+	int socket_servidor = -1;
+	int yes = 1;
 
 	struct addrinfo hints, *servinfo, *p;
 
@@ -16,81 +26,65 @@ int iniciar_servidor(void)
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
+	int rv = getaddrinfo(NULL, PUERTO, &hints, &servinfo);
+	if (rv != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		abort();
+	}
 
-	// Creamos el socket de escucha del servidor
+	for (p = servinfo; p != NULL; p = p->ai_next) {
 
-	// Asociamos el socket a un puerto
+		// Creamos el socket de escucha del servidor
+		socket_servidor = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+		if (socket_servidor == -1) {
+			continue;
+		}
+		setsockopt(socket_servidor, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
 
-	// Escuchamos las conexiones entrantes
+		// Asociamos el socket a un puerto
+		if (bind(socket_servidor, p->ai_addr, p->ai_addrlen) == -1) {
+			close(socket_servidor);
+			socket_servidor = -1;
+			continue;
+		}
+
+		// Escuchamos las conexiones entrantes
+		if (listen(socket_servidor, SOMAXCONN) == -1) {
+			close(socket_servidor);
+			socket_servidor = -1;
+			continue;
+		}
+
+		// si llegamos acá: socket listo
+		break;
+	}
 
 	freeaddrinfo(servinfo);
-	log_trace(logger, "Listo para escuchar a mi cliente");
 
+	if (socket_servidor == -1) {
+		fprintf(stderr, "No pude iniciar el servidor en el puerto %s\n", PUERTO);
+		abort();
+	}
+
+	log_trace(logger, "Listo para escuchar a mi cliente");
 	return socket_servidor;
 }
 
 int esperar_cliente(int socket_servidor)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	// assert(!"no implementado!");
 
 	// Aceptamos un nuevo cliente
-	int socket_cliente;
+	struct sockaddr_storage cliente_addr;
+	socklen_t addr_size = sizeof(cliente_addr);
+
+	int socket_cliente = accept(socket_servidor, (struct sockaddr*)&cliente_addr, &addr_size);
+	if (socket_cliente == -1) {
+		perror("accept");
+		abort();
+	}
+
 	log_info(logger, "Se conecto un cliente!");
-
 	return socket_cliente;
-}
-
-int recibir_operacion(int socket_cliente)
-{
-	int cod_op;
-	if(recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0)
-		return cod_op;
-	else
-	{
-		close(socket_cliente);
-		return -1;
-	}
-}
-
-void* recibir_buffer(int* size, int socket_cliente)
-{
-	void * buffer;
-
-	recv(socket_cliente, size, sizeof(int), MSG_WAITALL);
-	buffer = malloc(*size);
-	recv(socket_cliente, buffer, *size, MSG_WAITALL);
-
-	return buffer;
-}
-
-void recibir_mensaje(int socket_cliente)
-{
-	int size;
-	char* buffer = recibir_buffer(&size, socket_cliente);
-	log_info(logger, "Me llego el mensaje %s", buffer);
-	free(buffer);
-}
-
-t_list* recibir_paquete(int socket_cliente)
-{
-	int size;
-	int desplazamiento = 0;
-	void * buffer;
-	t_list* valores = list_create();
-	int tamanio;
-
-	buffer = recibir_buffer(&size, socket_cliente);
-	while(desplazamiento < size)
-	{
-		memcpy(&tamanio, buffer + desplazamiento, sizeof(int));
-		desplazamiento+=sizeof(int);
-		char* valor = malloc(tamanio);
-		memcpy(valor, buffer+desplazamiento, tamanio);
-		desplazamiento+=tamanio;
-		list_add(valores, valor);
-	}
-	free(buffer);
-	return valores;
 }
