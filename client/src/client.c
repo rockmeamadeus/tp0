af77c1f5dad003d995f63c94cdb14ae29203f2f2
rockmeamadeus <avaca@frba.utn.edu.ar>
2025-08-17 17:54:33 -0300

    Improves config reading and resource cleanup
    Adds proper logging of configuration values, ensures messages are sent to the server, and enhances memory management by freeing resources after use. Strengthens reliability and clarity of program execution.

--- client/src/client.c
+++ client/src/client.c
@@ -18,7 +18,7 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
+	log_info(logger, "Hola! Soy un log");
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
@@ -27,8 +27,12 @@ int main(void)
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
-	// Loggeamos el valor de config
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
 
+	// Loggeamos el valor de config
+	log_info(logger, "IP: %s, Puerto: %s, Valor: %s", ip, puerto, valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -42,6 +46,8 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
+
+	enviar_mensaje(conexion, valor);
 
 	// Armamos y enviamos el paquete
 	paquete(conexion);
@@ -74,9 +80,15 @@ void leer_consola(t_log* logger)
 	leido = readline("> ");
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
+	while (strcmp(leido, "") != 0)
+	{
+		log_info(logger, "Leído de consola: %s", leido);
+		free(leido);
+		leido = readline("> ");
+	}
 
 	// ¡No te olvides de liberar las lineas antes de regresar!
+	free(leido);
 
 }
 
@@ -88,13 +100,25 @@ void paquete(int conexion)
 
 	// Leemos y esta vez agregamos las lineas al paquete
 
+	paquete = crear_paquete();
+
+	while (strcmp(leido, "") != 0)
+	{
+		agregar_linea_paquete(paquete, leido);
+		free(leido);
+		leido = readline("> ");
+	}
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	
+	free(leido);
+	liberar_paquete(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	  liberar_conexion(conexion);
+	  log_destroy(logger);
+	  config_destroy(config);
 }
